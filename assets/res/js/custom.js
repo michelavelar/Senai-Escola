410dcd242d6a30188def3b18b31f16e8df3918bc
michelavelar <michel.avelar@gmail.com>
2021-12-08 17:39:58 -0300

    a

--- assets/res/js/custom.js
+++ assets/res/js/custom.js
@@ -1,5 +1,9 @@
 $("#menu-escola").click(function(){
     $(".navbar-header").toggleClass("active");
 });
-
  
+$(function() {
+    $("li").on("click", function(e) {  // See here, i have our selector set to "li", so this jQuery object will grab all li tags on the page
+        $(this).toggleClass("active");
+    });
+})
